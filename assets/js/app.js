b75d564bb362b2d2e0c06c06d4ec94378db39cd0
Benk0033 <benk0033@gmail.com>
2019-08-06 21:36:39 -0700

    adding clicky-game

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,34 @@
 $(document).ready(function ($) {
 
-    $(".scroll").click(function (event) {
-        event.preventDefault();
-        $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 900);
-    });
+    // $(".scroll").click(function (event) {
+    //     event.preventDefault();
+    //     $('html,body').animate({ scrollTop: $(this.hash).offset().top }, 900);
+    // });
 
-    $("span.menu").click(function () {
-        $(".top-menu ul").slideToggle("slow", function () {
+    // $("span.menu").click(function () {
+    //     $(".top-menu ul").slideToggle("slow", function () {
+    //     });
+    // });
+
+    var carousel = $(".carousel"),
+        currdeg = 0;
+
+    $(".next").on("click", { d: "n" }, rotate);
+    $(".prev").on("click", { d: "p" }, rotate);
+
+    function rotate(e) {
+        if (e.data.d == "n") {
+            currdeg = currdeg - 60;
+        }
+        if (e.data.d == "p") {
+            currdeg = currdeg + 60;
+        }
+        carousel.css({
+            "-webkit-transform": "rotateY(" + currdeg + "deg)",
+            "-moz-transform": "rotateY(" + currdeg + "deg)",
+            "-o-transform": "rotateY(" + currdeg + "deg)",
+            "transform": "rotateY(" + currdeg + "deg)"
         });
-    });
+    }
 
 })
